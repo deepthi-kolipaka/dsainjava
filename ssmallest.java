47cf89f60532c5f0cf6fe7fd790668decdcb5756
deepthi-kolipaka <deepthikolipakaaa@gmail.com>
2026-05-02 05:02:07 +0530

    Second smallest

---
+++ ssmallest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ssmallest {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        int smallest=arr[0];
+        int ssmallest=Integer.MAX_VALUE;
+        for(int i=1;i<n;i++){
+            if(arr[i]<smallest){
+                ssmallest=smallest;
+                smallest=arr[i];
+            }
+            else if(arr[i]!=smallest&&arr[i]<ssmallest){
+                ssmallest=arr[i];
+            }
+        }
+        System.out.println("Second smallest element in an array:"+ssmallest);
+
+    }
+}
