6eb4867e1c723c3e9d093582ed1115d740a1c8f0
deepthi-kolipaka <deepthikolipakaaa@gmail.com>
2026-05-02 05:06:03 +0530

    Second smallest

--- arrays/Secondsmallest.java
+++ arrays/Secondsmallest.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ssmallest {
+public class Secondsmallest {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         int n=in.nextInt();
