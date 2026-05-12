87b5e0450cfbe4572260bd45e2d2a5464480353e
deepthi-kolipaka <deepthikolipakaaa@gmail.com>
2026-05-12 20:21:53 +0530

    Move zeros to end in an array

--- Arrays/Move0.java
+++ Arrays/Move0.java
@@ -7,6 +7,7 @@ public class Move0 {
         for(int i=0;i<n;i++){
             arr[i]=in.nextInt();
         }
+        //using two pointers technique
         int j=0;
         for(int i=0;i<n;i++){
             if(arr[i]!=0){
