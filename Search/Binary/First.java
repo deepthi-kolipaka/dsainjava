605e0237f3f4fcde4d2d41c6e666287307ee275e
deepthi-kolipaka <deepthikolipakaaa@gmail.com>
2026-05-22 07:42:22 +0530

    Binary search theory

---
+++ Search/Binary/First.java
@@ -0,0 +1,13 @@
+package Search.Binary;
+public class First {
+    public static void main(String[] args) {
+        //binary search is for sorted arrays
+        /*algorithm :
+        1) find the middle element 
+        2)if->target elemt>mid => go with right side ,
+        else if->target elemt<mid=>go with leftside ,
+        else middle element is target elemnt 
+        3)
+        */
+    }
+}
