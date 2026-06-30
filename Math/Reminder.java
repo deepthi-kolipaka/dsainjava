eccdaf7f65b891f0e0fecb969b52a2b07bc33f14
deepthi-kolipaka <deepthikolipakaaa@gmail.com>
2026-06-30 20:10:13 +0530

    Dividing by 10

--- Math/Reminder.java
+++ Math/Reminder.java
@@ -1,9 +1,9 @@
 package Math;
 import java.util.Scanner;
-public class First{
+public class Reminder{
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         int n=in.nextInt();
-        System.out.println("Reminder is "+n%10);
+        System.out.println("Reminder when the value is divided by 10 "+n%10);
     }
 }
