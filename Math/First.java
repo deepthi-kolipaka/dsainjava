5d572cf7ee6f95f3b3f2e475b7feb8bd7cc262b5
deepthi-kolipaka <deepthikolipakaaa@gmail.com>
2026-06-30 20:06:26 +0530

    dividing by 10

---
+++ Math/First.java
@@ -0,0 +1,9 @@
+package Math;
+import java.util.Scanner;
+public class First{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        System.out.println("Reminder is "+n%10);
+    }
+}
