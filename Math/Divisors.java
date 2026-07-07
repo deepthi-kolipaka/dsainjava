b4f09bda56f2b6b5ae80e301cbd17eaa14383f72
deepthi-kolipaka <deepthikolipakaaa@gmail.com>
2026-07-07 14:40:55 +0530

    Math part 2

---
+++ Math/Divisors.java
@@ -0,0 +1,14 @@
+package Math;
+import java.util.*;
+public class Divisors {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
